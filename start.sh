3330cabd8487d148f712b481455d8ad31b2573d0
Ronald McCollam <mccollam@gmail.com>
2017-03-03 10:20:06 -0800

    added rotate parameter

--- start.sh
+++ start.sh
@@ -3,10 +3,11 @@
 URL=${URL:-"https://resin.io"}
 STRING=${STRING:-"I can git push to my name badge!"}
 NAME=${NAME:-"Ronald\nMcCollam"}
+ROTATE=${ROTATE:-0}
 
 qrencode -m 1 -o qr.png "$URL"
 convert bw.png -fill black -box white -gravity south -pointsize 14 -annotate +0+0 "$STRING" out.png
 composite -gravity northwest qr.png out.png out.png
-convert out.png -fill black -box white -gravity northeast -pointsize 14 -annotate +0+0 "$NAME" out.png
+convert out.png -fill black -box white -gravity northeast -pointsize 14 -annotate +0+0 "$NAME" -rotate $ROTATE out.png
 
 papirus-draw out.png
